fceb1c31e2bbd83ca367e1e3dd5f44da494e2b7e
StevenNet <26sthiriot@davis.k12.utu.s>
2021-12-11 15:40:05 -0700

    added fud and golden_fud to mod

--- src/main/java/com/stevennet/opossum/Opossum.java
+++ src/main/java/com/stevennet/opossum/Opossum.java
@@ -1,9 +1,11 @@
 package com.stevennet.opossum;
 
+import com.stevennet.opossum.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -30,13 +32,17 @@ public class Opossum
 
     public Opossum() {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
         // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        eventBus.addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        eventBus.addListener(this::processIMC);
         // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        eventBus.addListener(this::doClientStuff);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/com/stevennet/opossum/item/ModItems.java
+++ src/main/java/com/stevennet/opossum/item/ModItems.java
@@ -1,4 +1,36 @@
 package com.stevennet.opossum.item;
 
+import com.stevennet.opossum.Opossum;
+import net.minecraft.advancements.criterion.MobEffectsPredicate;
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Opossum.MOD_ID);
+
+
+    public static final RegistryObject<Item> FUD = ITEMS.register("fud",
+            () -> new Item(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(16)
+                    .food(new Food.Builder().hunger(6).saturation(6F).meat().fastToEat().build())));
+
+    public static final RegistryObject<Item> GOLDEN_FUD = ITEMS.register("golden_fud",
+            () -> new Item(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(16)
+                    .food(new Food.Builder().hunger(6).saturation(6F).meat().fastToEat().setAlwaysEdible()
+                            .effect(() -> new EffectInstance(Effects.ABSORPTION, 120*20, 2), 1F).build())));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
 }
